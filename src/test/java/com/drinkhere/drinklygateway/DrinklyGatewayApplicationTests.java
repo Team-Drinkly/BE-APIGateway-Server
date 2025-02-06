d560b872e475724681df856190c021e0f17a70e4
안정후 <austinan@naver.com>
2025-02-06 17:19:46 +0900

    chore: drinkly gateway server settings

---
+++ src/test/java/com/drinkhere/drinklygateway/DrinklyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinklyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
